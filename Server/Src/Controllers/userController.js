008f16e0c91a529a1f308eaec64c46272443230c
Mohamed Mohamud <mohammedmohamuud.01@gmail.com>
2024-01-10 14:55:46 +0300

    Built the login section and token generator

--- Server/Src/Controllers/userController.js
+++ Server/Src/Controllers/userController.js
@@ -1,30 +1,41 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
 const Prisma = require("../Config/Prisma");
+const bcrypt = require("bcryptjs");
 
 // @description Register new user
 // @Method POST
-// @Route /user/
+// @Route /userr/
 // @Access Public
 const register = asyncHandler(async (req, res) => {
+  // Fetch the necessary data from the request
   const { username, email, password } = req.body;
+
+  //Check if the email or password or username are provided
   if (!username || !email || !password) {
-    res.status(400);
-    throw new Error("Please provide all the required fields");
+    return res.status(400).json({
+      error: "Please provide all fields",
+    });
   }
+
+  //Check if the user already exists
   const user = await Prisma.user.findUnique({
     where: {
       email,
     },
   });
+
+  // If user already exists return a error message
   if (user) {
-    res.status(400);
-    throw new Error("User already exists");
+    return res.status(400).json({
+      error: "User already exists",
+    });
   }
 
+  // Hash the password
   const hashedPassword = await bcrypt.hash(password, 10);
 
+  // Create the user
   const newUser = await Prisma.user.create({
     data: {
       username,
@@ -33,11 +44,74 @@ const register = asyncHandler(async (req, res) => {
     },
   });
 
+  // Return the response with success and newly created user
   res.status(201).json({
     success: true,
-    message: "User created successfully",
-    user: newUser,
+    data: newUser,
+    token: generateToken(newUser.id, newUser.email),
   });
 });
 
-module.exports = { register };
+// @description Login
+// @Method POST
+// @Route /userr/login
+// @Access Public
+const login = asyncHandler(async (req, res) => {
+  // Fetch the necessary data from the request
+  const { email, password } = req.body;
+
+  //Check if the email or password or username are provided
+  if (!email || !password) {
+    res.status(400).json({
+      success: false,
+      error: "Please all the required fields!",
+    });
+  }
+
+  //Check if the user exists
+  const user = await Prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  // If user does not exist return a error message
+  if (!user) {
+    res.status(400).json({
+      success: false,
+      error: "User does not exist",
+    });
+  }
+
+  // Check if the password is correct
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  // If password is incorrect return a error message
+  if (!isMatch) {
+    res.status(400).json({
+      success: false,
+      error: "Invalid password",
+    });
+  }
+
+  // Return the response with success and newly created user
+  res.status(200).json({
+    success: true,
+    message: "User logged in successfully",
+    data: user,
+    token: generateToken(user.id, user.email),
+  });
+});
+
+const generateToken = (id, email) => {
+  const payload = { id, email };
+  const secret = process.env.JWT_SECRET;
+  return jwt.sign(payload, secret, {
+    expiresIn: "1d",
+  });
+};
+
+module.exports = {
+  register,
+  login,
+};
--- Server/Src/Routes/UserRoutes.js
+++ Server/Src/Routes/UserRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { register } = require("../Controllers/userController");
+const { register, login } = require("../Controllers/userController");
 const router = express.Router();
 
 router.post("/user/", register);
+router.post("/user/login", login);
 
 module.exports = router;
