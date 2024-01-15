2ed9aa0c9bf9f41c233cf7bbf729070e8b6ebde9
Mohamed <mohammedmohamuud.01@gmail.com>
2024-01-15 16:50:10 +0300

    Made the actiom  for creating new property by the owner

--- Server/Src/Controllers/PropertyController.js
+++ Server/Src/Controllers/PropertyController.js
@@ -1,14 +1,57 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
+const Prisma = require("../Config/Prisma");
 
 // @description Add Property
 // @Method POST
 // @Route /property/
 // @Access Public
 const addProperty = asyncHandler(async (req, res) => {
-  res.status(200).json({
+  const {
+    title,
+    city,
+    description,
+    price,
+    number_of_bedrooms,
+    number_of_kitchens,
+    number_of_toilets,
+  } = req.body;
+  const userId = req.user.id;
+
+  //Check if one of the required fields is empty
+  if (
+    !(
+      title ||
+      city ||
+      description ||
+      price ||
+      number_of_bedrooms ||
+      number_of_kitchens ||
+      number_of_toilets
+    )
+  ) {
+    return res.status(400).json({
+      error: "Please provide all fields",
+    });
+  }
+
+  const newProperty = await Prisma.Property.create({
+    data: {
+      userId,
+      title,
+      city,
+      description,
+      price: parseFloat(price),
+      number_of_bedrooms,
+      number_of_kitchens,
+      number_of_toilets,
+    },
+  });
+
+  res.status(201).json({
     success: true,
-    message: "Checking...",
+    message: "Property is created successfully",
+    data: newProperty,
   });
 });
 
--- Server/Src/Routes/PropertyRoutes.js
+++ Server/Src/Routes/PropertyRoutes.js
@@ -4,7 +4,8 @@ const Protect = require("../MiddleWares/authMiddleWare");
 const { isOwner, isRenter } = require("../MiddleWares/userRoleAuth");
 const router = express.Router();
 
-router.get("/", Protect, isRenter, addProperty);
+// Add new property
+router.post("/", Protect, isOwner, addProperty);
 
 // Export
 module.exports = router;
