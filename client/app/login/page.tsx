3b52bf47fb01ff2ff3576b7ba688612685fd7d8c
Mohamed <mohammedmohamuud.01@gmail.com>
2024-01-18 11:54:49 +0300

    Implemented login functionality and connected frontend to backend.

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -4,15 +4,11 @@ import React, { useState } from "react";
 import { Toaster, toast } from "sonner";
 
 const RegistrationPage = () => {
-  const apiUrl = "http://localhost:4321/user";
+  const apiUrl = "http://localhost:4321/user/login";
 
   const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    username: "",
     email: "",
     password: "",
-    role: "renter", // Assuming role is part of registration
   });
 
   const [loading, setLoading] = useState(false);
@@ -27,21 +23,18 @@ const RegistrationPage = () => {
     }));
   };
 
-  const handleRegistration = async (e: any) => {
+  const handleLogin = async (e: any) => {
     e.preventDefault();
 
     try {
       setLoading(true);
 
-      const response = await fetch(`${apiUrl}?role=${formData.role}`, {
+      const response = await fetch(apiUrl, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          firstName: formData.firstName,
-          lastName: formData.lastName,
-          username: formData.username,
           email: formData.email,
           password: formData.password,
         }),
@@ -50,15 +43,15 @@ const RegistrationPage = () => {
       const data = await response.json();
 
       if (response.ok) {
-        setSuccessMessage("Registration successful!");
+        setSuccessMessage("Logged In Successfully");
         toast.success(successMessage);
         // Optionally, you can redirect the user to another page on successful registration
       } else {
-        setError(data.error || "Registration failed.");
+        setError(data.error || "Login failed.");
         toast.error(error);
       }
     } catch (error) {
-      console.error("Error during registration:", error);
+      console.error("Error during login:", error);
       setError("An unexpected error occurred.");
     } finally {
       setLoading(false);
@@ -66,35 +59,11 @@ const RegistrationPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center mt-20">
+    <div className="flex flex-col items-center justify-center h-screen">
       <Toaster position="top-center" richColors />
 
       <div className="border-2 border-blue-500 rounded-lg p-8 bg-gray-100 shadow-md">
-        <form onSubmit={handleRegistration} className="space-y-4">
-          <input
-            value={formData.firstName}
-            name="firstName"
-            type="text"
-            className="border-2 p-3 w-full rounded-md"
-            placeholder="First Name"
-            onChange={handleInputChange}
-          />
-          <input
-            value={formData.lastName}
-            name="lastName"
-            type="text"
-            className="border-2 p-3 w-full rounded-md"
-            placeholder="Last Name"
-            onChange={handleInputChange}
-          />
-          <input
-            value={formData.username}
-            type="text"
-            name="username"
-            className="border-2 p-3 w-full rounded-md"
-            placeholder="Username"
-            onChange={handleInputChange}
-          />
+        <form onSubmit={handleLogin} className="space-y-4">
           <input
             value={formData.email}
             name="email"
@@ -111,18 +80,6 @@ const RegistrationPage = () => {
             placeholder="Password"
             onChange={handleInputChange}
           />
-          <div>
-            <select
-              value={formData.role}
-              name="role"
-              id="role"
-              className="border-2 p-3 w-full rounded-md"
-              onChange={handleInputChange}
-            >
-              <option value="renter">Renter</option>
-              <option value="owner">Owner</option>
-            </select>
-          </div>
           <button
             type="submit"
             className={`bg-blue-500 text-white px-4 py-2 rounded-md ${
@@ -130,15 +87,15 @@ const RegistrationPage = () => {
             }`}
             disabled={loading}
           >
-            {loading ? "Registering..." : "Register"}
+            {loading ? "Login..." : "Login"}
           </button>
         </form>
       </div>
       <div>
         <p>
-          Already Have Account?{" "}
-          <Link href={"/login"} className="text-sky-500">
-            Sign In
+          Don't have an account?{" "}
+          <Link href={"/signup"} className="text-sky-500">
+            Sign Up
           </Link>
         </p>
       </div>
