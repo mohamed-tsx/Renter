9e39d8ff2cbc95b9402dd6ee0c10a289d61c2b4a
Mohamed <mohammedmohamuud.01@gmail.com>
2024-01-20 20:48:26 +0300

    Installed Next JS again but this time with JavaScript not TypeScript

---
+++ client/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-100">
+      <h1>Started it again...</h1>
+    </div>
+  );
+}
